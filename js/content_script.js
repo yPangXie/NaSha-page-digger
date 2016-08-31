03b8537acbcbe7c99bdd6ff9a2a9980194151584
Mr.Krabs <pangxie.wdy@gmail.com>
2016-08-31 11:31:03 +0800

    refactor: 完善获取favicon的逻辑

--- js/content_script.js
+++ js/content_script.js
@@ -4,6 +4,7 @@
 (function() {
     let faviconLink = document.querySelector('link[rel="shortcut icon"]')
                     || document.querySelector('link[rel="short icon"]')
+                    || document.querySelector('link[rel="icon"]')
                     || document.querySelector('link[rel="apple-touch-icon"]')
                     || document.querySelector('link[rel="apple-touch-icon image_src"]');
     let faviconMeta = document.querySelector('meta[itemprop="image"]');
@@ -11,8 +12,8 @@
                     || document.querySelector('meta[name="twitter:description"]');
     let keywordsMeta = document.querySelector('meta[name="keywords"]');
     let titleElem = document.querySelector('title');
-    let favicon = faviconLink ? faviconLink.getAttribute('href') : (faviconMeta ? faviconMeta.getAttribute('content') : '');
     let url = window.location.href || '';
+    let favicon = faviconLink ? faviconLink.getAttribute('href') : (faviconMeta ? faviconMeta.getAttribute('content') : `${url}/favicon.ico`);
 
     /* 仅记录三种类型的页面数据. 其他的都无视. */
     if(!/^(https|http)/g.test(url)) return false;
