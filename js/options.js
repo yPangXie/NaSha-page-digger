5c487b7ea9a1278a880db9cb1b6cbb7697f7dfd9
Mr.Krabs <pangxie.wdy@gmail.com>
2016-09-07 10:46:59 +0800

    fix: 判断节点是否存在

--- js/options.js
+++ js/options.js
@@ -5,25 +5,29 @@ const STORAGE = chrome.storage.sync;
 STORAGE.get({
     blackList: ""
 }, result => {
-    document.querySelector('[data-role="black-list"]').value = result.blackList || '';
+    if(document.querySelector('[data-role="black-list"]')) document.querySelector('[data-role="black-list"]').value = result.blackList || '';
 });
 
 /* 保存数据 */
-document.querySelector('[data-role="save-black-list"]').addEventListener('click', () => {
-    let blackListData = document.querySelector('[data-role="black-list"]').value;
-    STORAGE.set({
-        blackList: blackListData || ''
-    }, result => {
-        alert('一般都是能顺利保存成功的.😎');
-    });
-}, false);
+if(document.querySelector('[data-role="save-black-list"]')) {
+    document.querySelector('[data-role="save-black-list"]').addEventListener('click', () => {
+        let blackListData = document.querySelector('[data-role="black-list"]').value;
+        STORAGE.set({
+            blackList: blackListData || ''
+        }, result => {
+            alert('一般都是能顺利保存成功的.😎');
+        });
+    }, false);
+}
 
 /* 清空数据 */
-document.querySelector('[data-role="clean-black-list"]').addEventListener('click', () => {
-    let confirmResult = confirm("确定清空了??? 清空可就什么都木有了🤔");
-    if(!confirmResult) return false;
+if(document.querySelector('[data-role="clean-black-list"]')) {
+    document.querySelector('[data-role="clean-black-list"]').addEventListener('click', () => {
+        let confirmResult = confirm("确定清空了??? 清空可就什么都木有了🤔");
+        if(!confirmResult) return false;
 
-    STORAGE.clear(result => {
-        document.querySelector('[data-role="black-list"]').value = '';
+        STORAGE.clear(result => {
+            document.querySelector('[data-role="black-list"]').value = '';
+        });
     });
-});
+}
